1b2ae1fb13a9ae3d059639338a8226b318e6d21b
Saurabh <Saurabh@LAPTOP-SG06UGT9>
2025-12-27 20:49:34 +0530

    Signature

---
+++ Priyadarshini/src/Signature.java
@@ -0,0 +1,20 @@
+
+public class Signature {
+	public static void main(String[] args) {
+		System.out.println("HEllo World");
+		Signature.main();
+	}
+	
+		public static void main(int[] args) {
+			System.out.println("Main accepting integer type array");
+		
+	}
+		public static void main() {
+			System.out.println("Main accepting any argument");
+		}
+		public static void main(char a) {
+			System.out.println("Main is accepting chaaracter input");
+		
+		}
+		 
+}
