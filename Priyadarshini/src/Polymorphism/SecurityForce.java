44cee1950b50e3a0ea99917315b94ec9aeaa706d
Saurabh <Saurabh@LAPTOP-SG06UGT9>
2025-12-30 20:37:02 +0530

    securityforce

---
+++ Priyadarshini/src/Polymorphism/SecurityForce.java
@@ -0,0 +1,30 @@
+package Polymorphism;
+
+
+public class SecurityForce {
+	
+	 void job() {
+		 System.out.println("To protect Nation");
+	 }
+	 void protect() {
+		System.out.println("Protect Country");
+		
+	}
+
+}
+class Army extends SecurityForce{
+	 void protect() {
+		System.out.println("Protect land");
+	}
+	void useTank() {
+		System.out.println("Army uses tanks");
+	}
+}
+class Navy extends SecurityForce{
+	 void protect() {
+		System.out.println("Protect ocean");
+	}
+		void useShips() {
+			System.out.println("Navy uses distroyers");
+		}
+}
